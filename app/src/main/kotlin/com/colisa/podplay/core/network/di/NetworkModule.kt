2c17e240950922924cef882aa4aa6695162f3686
mtali <emmanuelmtali@yahoo.com>
2024-06-22 00:51:09 +0300

    Add network di for json and okhttp client

--- app/src/main/kotlin/com/colisa/podplay/core/network/di/NetworkModule.kt
+++ app/src/main/kotlin/com/colisa/podplay/core/network/di/NetworkModule.kt
@@ -15,10 +15,39 @@
  */
 package com.colisa.podplay.core.network.di
 
+import com.colisa.podplay.BuildConfig
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NetworkModule
+object NetworkModule {
+
+  @Provides
+  @Singleton
+  fun providesNetworkJson(): Json = Json {
+    ignoreUnknownKeys = true
+  }
+
+  @Provides
+  @Singleton
+  fun okHttpCallFactory(): Call.Factory {
+    return OkHttpClient.Builder()
+      .addInterceptor(
+        HttpLoggingInterceptor()
+          .apply {
+            if (BuildConfig.DEBUG) {
+              setLevel(HttpLoggingInterceptor.Level.BODY)
+            }
+          },
+      )
+      .build()
+  }
+}
